d5114437174cc8861c849a7cbcd7d222f7b31f07
Yash Djson Dookun <dookun88@gmail.com>
2019-10-03 10:06:41 +0400

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,51 +1,50 @@
-$(document).ready(function () {
-	const titleText =  document.querySelector('.titleText');
-    const welcomeMsg =  document.querySelector('.welcomeMsg');
-    const followAlong =  document.querySelector('.followAlong');
+$(document).ready(function() {
+    const titleText = document.querySelector('.titleText');
+    const welcomeMsg = document.querySelector('.welcomeMsg');
+    const followAlong = document.querySelector('.followAlong');
     const aboutMein = document.querySelector('.aboutMe');
     const aboutMyCourseIn = document.querySelector('.aboutMyCourse');
-	const width = $(window).width();
+    const width = $(window).width();
 
-	if (width > 1666 && width > 768 ){
-		splitScroll();
-	}
+    if (width > 768) {
+        splitScroll();
+    }
 
-    $(window).scroll(function () {
+    $(window).scroll(function() {
         if ($(window).scrollTop()) {
-            $("header").css("background-color","rgba(10, 36, 47, 0.95)");
+            $("header").css("background-color", "rgba(10, 36, 47, 0.95)");
             $("header").addClass('sticky');
             aboutMein.classList.add('animated', 'slideInLeft');
-			aboutMyCourseIn.classList.add('animated', 'slideInRight');
-        }
-        else {
+            aboutMyCourseIn.classList.add('animated', 'slideInRight');
+        } else {
             $("header").css("background-color", "transparent");
             $("header").removeClass('sticky');
             aboutMein.classList.remove('animated', 'slideInLeft');
-			aboutMyCourseIn.classList.remove('animated', 'slideInRight');
+            aboutMyCourseIn.classList.remove('animated', 'slideInRight');
         }
     });
 
-	titleText.classList.add('animated', 'fadeInLeft');
-	welcomeMsg.classList.add('animated', 'slideInUp');
-	followAlong.classList.add('animated', 'zoomIn');
+    titleText.classList.add('animated', 'fadeInLeft');
+    welcomeMsg.classList.add('animated', 'slideInUp');
+    followAlong.classList.add('animated', 'zoomIn');
 
-	$(".followAlong-btn").click(function() {
-		$('html, body').animate({
-		    scrollTop: $(".aboutMe").offset().top
-		}, 1000)
-		aboutMein.classList.add('animated', 'slideInLeft');
-		aboutMyCourseIn.classList.add('animated', 'slideInRight');
-	});
+    $(".followAlong-btn").click(function() {
+        $('html, body').animate({
+            scrollTop: $(".aboutMe").offset().top
+        }, 1000)
+        aboutMein.classList.add('animated', 'slideInLeft');
+        aboutMyCourseIn.classList.add('animated', 'slideInRight');
+    });
 });
 
 function splitScroll() {
-	const controller = new ScrollMagic.Controller();
-	new ScrollMagic.Scene({
-		duration: '100%',
-		triggerElement: '.aboutMe',
-		triggerHook: 0
-	})
-	.setPin('.aboutMe')
-	// .addIndicators()
-	.addTo(controller);
+    const controller = new ScrollMagic.Controller();
+    new ScrollMagic.Scene({
+            duration: '100%',
+            triggerElement: '.aboutMe',
+            triggerHook: 0
+        })
+        .setPin('.aboutMe')
+        // .addIndicators()
+        .addTo(controller);
 }
